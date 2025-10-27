809599e603d4586fcd9dac73d89f0e5a25f44bcb
William <william.barnard4@yahoo.com>
2025-10-26 19:06:10 -0500

    First 3 Videos
    Add content to project from "Creating a Standard Target," "Adding a First-person Shooter," and "Adding a Muzzle Flash Effect."

---
+++ Assets/My Stuff/Scripts/Target.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    public GameObject target;
+
+    public GameObject effectsManager;
+    public GameObject hitEffect;
+    public float effectDuration = 0.1f;
+    public AudioClip hitSound;
+
+    //protected Effect effectScript;
+
+    private void Awake()
+    {
+        //effectScript = effectsManager.getComponent<Effect>();
+    }
+
+    public virtual void Process(RaycastHit hit)
+    {
+
+    }
+}
